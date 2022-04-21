6fd288ba4afb316f3cd5e404a0e61787cc9b260b
Nicolas G <niccgag@gmail.com>
2022-04-22 09:06:45 +1200

    initial commit

---
+++ src/components/CartContainer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
+
+const CartContainer = () => {
+  const { cartItems, total, amount } = useSelector((state) => state.cart);
+
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+  return <div>CartContainer</div>;
+};
+
+export default CartContainer;
